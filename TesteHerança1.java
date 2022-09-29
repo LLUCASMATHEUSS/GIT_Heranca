3291a01d872344b72a3885b825db9e81a1fa8d07
Lucas <martlucas.com.br>
2022-09-29 16:10:49 -0300

    heranca(apostila05): fim da listagem 1.3.2.2

---
+++ TesteHerança1.java
@@ -0,0 +1,10 @@
+public class TesteHerança1{
+    public static void main(String[] args){
+        //Vejamos se alunos de graduação tem nome e idade
+        //construtor que não recebe nada: construtor padrão
+        AlunoDeGraduacao aluno = new AlunoDeGraduacao();
+        aluno.nome = "joão";
+        aluno.idade = 17;
+        System.out.printf("nome %s, idade: %d\n", aluno.nome, aluno.idade);
+    }
+}
